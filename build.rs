51617af3976935282a2ef19efc18a930762c5640
Tray Torrance <torrancew@gmail.com>
2024-07-08 17:11:47 -0700

    Silence Xapian deprecation warnings for auto-generated bindings

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() -> miette::Result<()> {
     let local = std::path::PathBuf::from("cpp");
     let mut b = autocxx_build::Builder::new("src/ffi.rs", [&local]).build()?;
-    b.flag_if_supported("-std=c++14").compile("xapian-rs");
+    b.flag_if_supported("-std=c++14")
+        .flag_if_supported("-Wno-deprecated-declarations")
+        .compile("xapian-rs");
 
     println!("cargo:rerun-if-changed=cpp/shim.h");
     println!("cargo:rerun-if-changed=src/ffi.rs");
