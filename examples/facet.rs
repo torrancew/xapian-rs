c3e5264ceff9d586668318467f022fead98ec8a2
Tray Torrance <torrancew@gmail.com>
2024-12-20 16:08:56 -0800

    Add a more nuanced facet example

--- examples/facet.rs
+++ examples/facet.rs
@@ -37,6 +37,34 @@ impl<T: FromValue + Ord> MatchSpy for ValueSpy<T> {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct BucketingValueSpy<T: PartialEq, F> {
+    bucket_fn: F,
+    slot: xapian_rs::Slot,
+    stats: Rc<RefCell<BTreeMap<T, usize>>>,
+}
+
+impl<T: FromValue + Ord, F: Fn(&T) -> T> BucketingValueSpy<T, F> {
+    pub fn new(slot: impl Into<xapian_rs::Slot>, bucket_fn: F) -> Self {
+        Self {
+            bucket_fn,
+            slot: slot.into(),
+            stats: Default::default(),
+        }
+    }
+}
+
+impl<T: FromValue + Ord, F: Fn(&T) -> T> MatchSpy for BucketingValueSpy<T, F> {
+    fn observe(&self, doc: &xapian_rs::Document, _: f64) {
+        if let Some(Ok(key)) = doc.value::<T>(self.slot) {
+            let bucket_key = (self.bucket_fn)(&key);
+            let mut stats = self.stats.borrow_mut();
+            let count = stats.entry(bucket_key).or_insert(0);
+            *count += 1;
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let stemmer = Stem::for_language("english");
 
@@ -45,6 +73,7 @@ fn main() -> anyhow::Result<()> {
     let qstr = args.queries.join(" ");
 
     let spy = ValueSpy::<u32>::new(1);
+    let bucket_spy = BucketingValueSpy::<u32, _>::new(1, |x| (x / 100) * 100);
     let mut qp = QueryParser::default();
     qp.add_prefix("description", "XD:");
 
@@ -65,9 +94,14 @@ fn main() -> anyhow::Result<()> {
     let mut enquire = Enquire::new(db);
     enquire.set_query(&query, None);
     enquire.add_matchspy(&spy);
+    enquire.add_matchspy(&bucket_spy);
     let results = enquire.mset(0, 100, 100, None, None);
     for (key, count) in spy.stats.borrow().iter() {
         eprintln!("spy:{key}={count}")
+    }
+
+    for (key, count) in bucket_spy.stats.borrow().iter() {
+        eprintln!("bucket_spy:{key}={count}")
     }
 
     for m in results.matches() {
